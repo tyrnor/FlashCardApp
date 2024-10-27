8f87cf66538206c1b8873800cca7de2b81e99ff6
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-27 22:34:27 +0100

    Delete Resource

--- app/src/main/java/com/example/flashcardapp/domain/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.flashcardapp.domain
-
-sealed class Resource<out T> {
-    data class Success<out T>(val data: T) : Resource<T>()
-    data class Error(val message: String) : Resource<Nothing>()
-    data object Loading : Resource<Nothing>()
-}
