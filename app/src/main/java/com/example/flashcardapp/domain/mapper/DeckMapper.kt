a0dd01cf1b3fe92ef78eeb58f61795fc7be93a01
carlosyanezpuig <carlosyanez997@gmail.com>
2024-10-27 15:58:00 +0100

    Create DeckMapper utility

---
+++ app/src/main/java/com/example/flashcardapp/domain/mapper/DeckMapper.kt
@@ -0,0 +1,21 @@
+package com.example.flashcardapp.domain.mapper
+
+import com.example.flashcardapp.data.model.DeckDto
+import com.example.flashcardapp.domain.model.Deck
+
+class DeckMapper {
+
+    fun toDomain(deckDto: DeckDto): Deck {
+        return Deck(
+            id = deckDto.id,
+            name = deckDto.name,
+        )
+    }
+
+    fun toDto(deck: Deck): DeckDto {
+        return DeckDto(
+            id = deck.id,
+            name = deck.name,
+        )
+    }
+}
