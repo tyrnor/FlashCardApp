76566c614efa7196068c2954e5ed67c8ad6d582a
carlosyanezpuig <carlosyanez997@gmail.com>
2024-11-03 15:13:50 +0100

    Create getDeckCards function in FirestoreRepository

--- app/src/main/java/com/example/flashcardapp/domain/repository/FirestoreRepository.kt
+++ app/src/main/java/com/example/flashcardapp/domain/repository/FirestoreRepository.kt
@@ -1,5 +1,6 @@
 package com.example.flashcardapp.domain.repository
 
+import com.example.flashcardapp.domain.model.Card
 import com.example.flashcardapp.domain.model.Deck
 import kotlinx.coroutines.flow.Flow
 
@@ -8,5 +9,6 @@ interface FirestoreRepository {
     suspend fun getUserDecks(uid: String) : Flow<Result<List<Deck>>>
     suspend fun addDeck(uid: String, deck: Deck) : Result<Unit>
     suspend fun deleteDeck(uid: String, deckId: String) : Result<Unit>
+    suspend fun getDeckCards(uid: String, deckId: String) : Flow<Result<List<Card>>>
 
 }
