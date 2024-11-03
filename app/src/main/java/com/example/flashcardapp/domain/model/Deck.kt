51f72919dfc6111cb460e163f741e1e281d7ecc6
carlosyanezpuig <carlosyanez997@gmail.com>
2024-11-03 14:29:18 +0100

    Modify deck model

--- app/src/main/java/com/example/flashcardapp/domain/model/Deck.kt
+++ app/src/main/java/com/example/flashcardapp/domain/model/Deck.kt
@@ -3,7 +3,7 @@ package com.example.flashcardapp.domain.model
 import com.google.firebase.Timestamp
 
 data class Deck(
-    val id: String,
+    val id: String = "",
     val name: String,
     val timestamp: Timestamp
 )
